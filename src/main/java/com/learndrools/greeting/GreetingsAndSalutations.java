328095e98cf9d276fadb03c839524d144e7a9705
Brian Sam-Bodden <bsbodden@integrallis.com>
2019-03-15 15:23:27 -0700

    feat: fix cardinality issue / improving the work product GreetingsAndSalutations

--- src/main/java/com/learndrools/greeting/GreetingsAndSalutations.java
+++ src/main/java/com/learndrools/greeting/GreetingsAndSalutations.java
@@ -2,12 +2,14 @@ package com.learndrools.greeting;
 
 public class GreetingsAndSalutations {
     private Person recipient;
+    private TimeOfDay tod;
     private String greeting;
     private String prenomial;
     private String postnomial;
 
-    public GreetingsAndSalutations(Person recipient, String greeting) {
+    public GreetingsAndSalutations(Person recipient, TimeOfDay tod, String greeting) {
         this.recipient = recipient;
+        this.tod = tod;
         this.greeting = greeting;
     }
 
@@ -43,6 +45,14 @@ public class GreetingsAndSalutations {
         this.postnomial = postnomial;
     }
     
+    public TimeOfDay getTod() {
+        return tod;
+    }
+
+    public void setTod(TimeOfDay tod) {
+        this.tod = tod;
+    }
+
     @Override
     public String toString() {
         String result = getGreeting() + getPrenomial() + " " + getRecipient().getName();
